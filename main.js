064775a607bf0c3c77ac5b31a39543b2f1b9d79a
Alejandro <aleig.906@gmail.com>
2022-12-19 18:25:51 -0300

    renderizacion escucha

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
 const recognition = new webkitSpeechRecognition()
-recognition.lang = 'es-ES'
+recognition.lang = 'es-AR'
+
 recognition.continuous = true
+
+const contEsc = document.getElementById('escuchadoCont')
+
+const contenedorEscuchando = document.getElementById('contenedorEscuchando')
+
+const selectLang = document.getElementById('selectLang')
+
+function limpiarescuchar() {
+  contEsc.innerHTML = ''
+}
+
+function escuchar() {
+
+console.log(selectLang.value)
+
+recognition.start()
+
+contenedorEscuchando.innerHTML = ''
+
+let tit = document.createElement('h1')
+
+tit.innerHTML = 'Listening.'
+
+contenedorEscuchando.appendChild(tit)
+
 recognition.onresult = event => {
   for (const result of event.results) {
-    console.log(result[0].transcript)
+    contEsc.innerHTML = ''
+    let escuchado = document.createElement('p')
+    escuchado.innerHTML = result[0].transcript
+    contEsc.appendChild(escuchado)
   }
 }
-recognition.start()
+
+}
+
+function dejardeescuchar() {
+console.log("Not listening.")
+recognition.stop()
+contenedorEscuchando.innerHTML = ''
+let tit = document.createElement('h1')
+tit.innerHTML = 'Not listening.'
+contenedorEscuchando.appendChild(tit)
+}
 
 
 
